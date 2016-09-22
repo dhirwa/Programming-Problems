68480553f8c951920c63e90c293477cae0ba8952
hirwa dieume <dhirwa@exuus.com>
2016-09-22 17:06:20 +0200

    Commit on 3rd Q

---
+++ prob2/prob2.js
@@ -0,0 +1,48 @@
+function createList(num){
+  var i=0;
+  var list= [];
+  i=1;
+  while(i <= num){
+    list.push(Math.floor(Math.random()*100));
+    i++;
+  }
+  return list;
+}
+
+function calculateAverage(list){
+  var averageValue=0;
+  var i=0;
+  while(i < list.length){
+    averageValue+= Number(list[i]);
+    i++;
+  }
+  return Math.round(averageValue/list.length);
+}
+
+function calculateVariance(list, average){
+  var i=0;
+  var varianceValue=0;
+  while(i < list.length){
+
+    varianceValue+=Number(Math.pow(list[i]- avg, 2));
+    i++;
+  }
+  return varianceValue/list.length;
+}
+
+function calculateStandardDeviation(variance){
+  var stdDev= Math.sqrt(variance);
+  return Math.round(stdDev);
+}
+
+var list = createList(10);
+console.log('The numbers are ' + list);
+
+var avg = calculateAverage(list);
+console.log('The average is ' + avg);
+
+var variance = calculateVariance(list, avg);
+console.log('The variance is ' + variance);
+
+stdDev = calculateStandardDeviation(variance);
+console.log('The standard dev is ' + stdDev);
